a0e37a39856b01ce86d9dfda6ec69d77608e9115
tsmsogn <tsmsogn@gmail.com>
2016-10-12 09:43:24 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+group :development, :test do
+  gem 'rspec'
+  gem 'capybara'
+  gem 'capybara-webkit'
+  gem "poltergeist"
+end
---
+++ spec/test_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+Capybara.javascript_driver = :poltergeist
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, js_errors: true, timeout: 30)
+end
+
+Capybara.configure do |config|
+  config.run_server = false
+  config.default_driver = :poltergeist
+  config.app_host = 'http://tsmsogn.github.io'
+end
+
+describe '/', type: :feature do
+  subject { page }
+  before { visit('/') }
+
+  it 'Title' do
+    expect(page).to have_title 'aaaa'
+  end
+end
